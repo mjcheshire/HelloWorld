e1d0cf183286b06ab5c42cbd54d1ee63aba3372c
MattC <MattC@192.168.1.211>
2025-12-05 11:26:23 +0000

    Hello World

---
+++ HelloWorld/src/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int zzz = 34;
+		System.out.println("Hello, World");
+		System.out.flush();
+		System.out.println("xxxxx" + zzz);
+	}
+
+}
